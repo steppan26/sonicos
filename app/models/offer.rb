5429bb1debd679c2364fb1c0261224d0c4f9d3bb
Stephane Baroux <stephane.baroux@gmail.com>
2021-11-15 16:04:18 +0100

    removed contant variable and moved the array directly in the validation

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,9 +1,7 @@
 class Offer < ApplicationRecord
-  OFFER_STATUS = ['pending', 'accepted', 'rejected']
-
   belongs_to :user
   belongs_to :costume
   validates :date_of_hire, :duration, presence: true
   validates :duration, numericality: { only_integer: true }
-  validates :status, incusion: STATUS
+  validates :status, incusion: ['pending', 'accepted', 'rejected']
 end
