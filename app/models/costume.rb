46efb455e2ad97bbaa8a917b148d485617f54276
Stephane Baroux <stephane.baroux@gmail.com>
2021-11-15 15:11:12 +0100

    added validations to models user, costume and offer

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,3 +1,7 @@
 class Costume < ApplicationRecord
   belongs_to :user
+  has_many :offers, dependent: :destroy
+
+  validates :title, :image_url, :price, :location, presence: true
+  validates :title, length: { minimum: 5, maximum: 60 }
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,9 @@
 class Offer < ApplicationRecord
+  STATUS = ['pending', 'accepted', 'rejected']
+
   belongs_to :user
   belongs_to :costume
+  validates :date_of_hire, :duration, presence: true
+  validates :duration, numericality: { only_integer: true }
+  validates :status, incusion: STATUS
 end
