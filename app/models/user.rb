165ce735afe140da40814fd07d291b3275385bdd
gauthier chabanne <gauthierchabanne@hotmail.fr>
2021-11-16 10:47:39 +0100

    Started devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :costumes, dependent: :destroy
   has_many :offers, dependent: :destroy
   validates :username, uniqueness: true, presence: true
