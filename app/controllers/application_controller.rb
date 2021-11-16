b1c467100e46781d9c19006af7343830a4d8ec4d
gauthier chabanne <gauthierchabanne@hotmail.fr>
2021-11-16 14:39:52 +0100

    better cloudinary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::Base
       # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username])
   end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || dashboard_path
+  end
+
 end
