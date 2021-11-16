8c650b02e3aba26834500eca8700a2ac0678d2a8
Stephane Baroux <stephane.baroux@gmail.com>
2021-11-16 15:25:54 +0100

    Revert "compatibility problems"
    This reverts commit e8cc56754f2d9c97698fe2f26dfe0bf08374ac0a, reversing
    changes made to 59b51b01549bf86a7be9b37b4fd097ead2adfedd.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,9 +20,4 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
-  
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || dashboard_path
-  end
-
 end
