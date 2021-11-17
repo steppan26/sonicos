296e91184d95ab070fed02d4a0d3a51a243bc75b
arthur de bailliencourt <arthur.de.bailliencourt@gmail.com>
2021-11-17 13:23:40 +0100

    offer tab done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,10 @@ class PagesController < ApplicationController
     @my_costumes = Costume.where(user: current_user)
     @costumes = Costume.all
     @costume = Costume.new
+    @my_offers = Offer.where(user_id: current_user.id)
+    @my_costumes_offers = []
+    @my_offers.each do |offer|
+      @my_costumes_offers << Costume.find(offer.costume_id)
+    end
   end
 end
