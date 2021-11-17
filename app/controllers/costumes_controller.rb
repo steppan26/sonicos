c991dff1cbbb1f8cd70d19a35b5af2c3e7ef98c1
gauthier chabanne <gauthierchabanne@hotmail.fr>
2021-11-17 11:22:27 +0100

    Mappartialy added

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -19,6 +19,7 @@ class CostumesController < ApplicationController
   def show
     @costume = Costume.find(params[:id])
     @user = current_user
+    @offer = Offer.new
     authorize @costume
   end
 
