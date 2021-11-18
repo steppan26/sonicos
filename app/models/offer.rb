d7b471e42c29864c9e571ffc74976da5376fc7ab
arthur de bailliencourt <arthur.de.bailliencourt@gmail.com>
2021-11-18 14:42:39 +0100

    done

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,4 +4,8 @@ class Offer < ApplicationRecord
   validates :date_of_hire, :duration, presence: true
   validates :duration, numericality: { only_integer: true }
   validates :status, inclusion: ['pending', 'accepted', 'rejected']
+
+  def date_of_end
+    date_of_hire + duration
+  end
 end
