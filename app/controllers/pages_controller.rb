3c96a327a66518d344f179ca9135aa3d67b1920d
gauthier chabanne <gauthierchabanne@hotmail.fr>
2021-11-19 12:19:42 +0100

    Almost done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,6 +16,15 @@ class PagesController < ApplicationController
     end
   end
 
-  def test
+  def test1
+    @active_tab = params[:page] || "homepage"
+    @my_costumes = Costume.where(user: current_user)
+    @costumes = Costume.all
+    @costume = Costume.new
+    @my_offers = Offer.where(user_id: current_user.id)
+    @my_costumes_offers = []
+    @my_offers.each do |offer|
+    @my_costumes_offers << Costume.find(offer.costume_id)
+    end
   end
 end
